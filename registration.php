523f8fbef30b1a83d7862b29ed8b49f0d8114ecf
pournimas7693 <pournima.shinde@neosoftmail.com>
2022-06-08 14:48:44 +0530

    registration changes

--- registration.php
+++ registration.php
@@ -7,12 +7,11 @@ if(isset($_POST['register'])){
     $_SESSION["email"] = $email;
     $phone=$_POST['phone'];
     $pass=$_POST['password'];   
-    $age=$_POST['age'];
-    $city=$_POST['city'];
+    $age=$_POST['age'];   
     $tmp=$_FILES['att']['tmp_name'];
     $fname=$_FILES['att']['name'];
     $ext=pathinfo($fname,PATHINFO_EXTENSION);
-    if(empty($email) || empty($pass) || empty($name || empty($age) || empty($city) || empty($phone) || empty($tmp))){
+    if(empty($email) || empty($pass) || empty($name || empty($age)  || empty($phone) || empty($tmp))){
         $errMsg="Please fill the blank fields";
     }
     else if(!preg_match("/^[a-zA-Z ]+$/",$name)){
@@ -33,7 +32,7 @@ if(isset($_POST['register'])){
                if(move_uploaded_file($tmp,"users/$email/$email".".jpg"))//upload image
                {
               
-               file_put_contents("users/$email/details.txt","$name\n$pass\n$age\n$city\n$phone");//create file and store details
+               file_put_contents("users/$email/details.txt","$name\n$pass\n$age\n$phone");//create file and store details
                header("location:welcome.php");//redirect to login
                }
                else {
@@ -72,24 +71,19 @@ if(isset($_POST['register'])){
             <form method="post" enctype="multipart/form-data">
             <div class="form-group">
                     <label> Full Name </label>
-                    <input type="text" name="name" class="form-control"/>
-                 <?php
-               if(isset($nameErr))
-               {
-                 ?>
-            <div class="alert alert-danger"><?= $nameErr;?></div>
-                 <?php 
-               }
-               ?>
-                </div>
+                    <input id="name" type="text" name="name" class="form-control" onkeyup="Validatename();"/>
+                    <span id="nError" style="color: red"></span>
+                    </div>
                 <div class="form-group">
                     <label> Email </label>
-                    <input type="email" name="email" class="form-control"/>
+                    <input id="email" type="email" name="email" class="form-control" onkeyup="ValidateEmail();"/>
+                    <span id="eError" style="color: red"></span>
                 </div>
                 <div class="form-group">
                 <label> Phone Number </label>
-                     <input type="text" name="phone" class="form-control" placeholder="+91 98196 23580" aria-label="Phone">
-                </div>
+                     <input id="phone" type="text" name="phone" class="form-control" placeholder="" aria-label="Phone" onkeyup="Validatephone();">
+               <span id="pError" style="color: red"></span>
+               </div>
                 <div class="form-group">
                     <label> Password </label>
                     <input type="password" name="password" class="form-control"/>
@@ -98,10 +92,7 @@ if(isset($_POST['register'])){
                     <label> Age </label>
                     <input type="text" name="age" class="form-control"/>
                 </div>
-                <div class="form-group">
-                    <label> City </label>
-                    <input type="text" name="city" class="form-control"/>
-                </div>               
+                              
                 <div class="form-group">
                     <label> Image </label>
                     <input type="file" name="att" class="form-control"/>
@@ -114,6 +105,43 @@ if(isset($_POST['register'])){
         </section>
     </main>    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   <!--Client side validation-->
+   <script type="text/javascript">
+    function ValidateEmail() {
+        var email = document.getElementById("email").value;
+        var lblError = document.getElementById("eError");
+        lblError.innerHTML = "";
+        var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        if (!expr.test(email)) {
+            lblError.innerHTML = "Invalid email address.";
+        }
+    }
+
+    //Only chaeacters
+
+    function Validatename() {
+        var email = document.getElementById("name").value;
+        var lblError = document.getElementById("nError");
+        lblError.innerHTML = "";
+        var expr =/^[a-zA-Z ]+$/;
+        if (!expr.test(email)) {
+            lblError.innerHTML = "Only alphabates allowed.";
+        }
+    }
+
+    //Phone number
+
+    function Validatephone() {
+        var phone = document.getElementById("phone").value;
+        var lblError = document.getElementById("pError");
+        lblError.innerHTML = "";
+        var expr =/^\d{10}$/;
+        if (!expr.test(phone)) {
+            lblError.innerHTML = "Please enter 10 digit number";
+        }
+    }
+
+</script>
 
   </body>
 </html>
